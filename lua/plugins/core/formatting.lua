12f9006b18846d8182c919c498315e4aa3bb0b77
Harry Min <harryhmin@gmail.com>
2024-03-03 10:51:08 +0000

    conform don't notify error

--- lua/plugins/core/formatting.lua
+++ lua/plugins/core/formatting.lua
@@ -16,8 +16,8 @@ return {
       },
     },
     opts = {
+      notify_on_error = false,
       format_on_save = {
-        -- I recommend these options. See :help conform.format for details.
         lsp_fallback = true,
         timeout_ms = 500,
       },
