fc26f1e96db5aa0d9a1c83e62f74935ac679881a
Harry Min <harryhmin@gmail.com>
2024-03-08 10:55:29 +0000

    change cmp sort order

--- lua/plugins/core/cmp.lua
+++ lua/plugins/core/cmp.lua
@@ -49,6 +49,19 @@ return {
     opts = function(_, opts)
       local luasnip = require("luasnip")
       local cmp = require("cmp")
+      local types = require("cmp.types")
+
+      ---@type table<integer, integer>
+      local modified_priority = {
+        [types.lsp.CompletionItemKind.Variable] = types.lsp.CompletionItemKind.Method,
+        [types.lsp.CompletionItemKind.Snippet] = 0, -- top
+        [types.lsp.CompletionItemKind.Keyword] = 0, -- top
+        [types.lsp.CompletionItemKind.Text] = 100, -- bottom
+      }
+      ---@param kind integer: kind of completion entry
+      local function modified_kind(kind)
+        return modified_priority[kind] or kind
+      end
 
       local M = {
         snippet = {
@@ -83,20 +96,65 @@ return {
           { name = "path", priority = 250 },
           { name = "emoji" },
         }),
+        -- https://github.com/pysan3/dotfiles/blob/9d3ca30baecefaa2a6453d8d6d448d62b5614ff2/nvim/lua/plugins/70-nvim-cmp.lua#L132-L162
         sorting = {
-          priority_weight = 1.0,
+          -- https://github.com/hrsh7th/nvim-cmp/blob/main/lua/cmp/config/compare.lua
           comparators = {
-            cmp.config.compare.exact,
-            cmp.config.compare.locality,
-            cmp.config.compare.recently_used,
-            cmp.config.compare.score, -- based on :  score = score + ((#sources - (source_index - 1)) * sorting.priority_weight)
             cmp.config.compare.offset,
+            cmp.config.compare.exact,
+            function(entry1, entry2) -- sort by length ignoring "=~"
+              local len1 = string.len(string.gsub(entry1.completion_item.label, "[=~()_]", ""))
+              local len2 = string.len(string.gsub(entry2.completion_item.label, "[=~()_]", ""))
+              if len1 ~= len2 then
+                return len1 - len2 < 0
+              end
+            end,
+            cmp.config.compare.recently_used,
+            function(entry1, entry2) -- sort by cmp.config.compare kind (Variable, Function etc)
+              local kind1 = modified_kind(entry1:get_kind())
+              local kind2 = modified_kind(entry2:get_kind())
+              if kind1 ~= kind2 then
+                return kind1 - kind2 < 0
+              end
+            end,
+            function(entry1, entry2) -- score by lsp, if available
+              local t1 = entry1.completion_item.sortText
+              local t2 = entry2.completion_item.sortText
+              if t1 ~= nil and t2 ~= nil and t1 ~= t2 then
+                return t1 < t2
+              end
+            end,
+            cmp.config.compare.score,
             cmp.config.compare.order,
-            cmp.config.compare.scopes, -- what?
-            cmp.config.compare.sort_text,
-            cmp.config.compare.kind,
           },
         },
+        -- sorting = {
+        --   comparators = {
+        --     cmp.config.compare.offset,
+        --     cmp.config.compare.exact,
+        --     cmp.config.compare.score,
+        --     cmp.config.compare.recently_used,
+        --     cmp.config.compare.locality,
+        --     cmp.config.compare.kind,
+        --     cmp.config.compare.sort_text,
+        --     cmp.config.compare.length,
+        --     cmp.config.compare.order,
+        --   },
+        -- },
+        -- sorting = {
+        --   priority_weight = 1.0,
+        --   comparators = {
+        --     cmp.config.compare.exact,
+        --     cmp.config.compare.locality,
+        --     cmp.config.compare.recently_used,
+        --     cmp.config.compare.score, -- based on :  score = score + ((#sources - (source_index - 1)) * sorting.priority_weight)
+        --     cmp.config.compare.offset,
+        --     cmp.config.compare.order,
+        --     cmp.config.compare.scopes, -- what?
+        --     cmp.config.compare.sort_text,
+        --     cmp.config.compare.kind,
+        --   },
+        -- },
 
         mapping = {
           ["<Up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
@@ -109,7 +167,7 @@ return {
           ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
           ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
           ["<C-e>"] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close() }),
-          ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+          -- ["<C-y>"] = cmp.mapping.confirm({ select = true }),
           ["<CR>"] = cmp.mapping.confirm({ select = false }),
 
           ["<Tab>"] = cmp.mapping(function(fallback)
