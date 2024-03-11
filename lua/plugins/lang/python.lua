dd40fb4d85a5b96ba30fbbb8022dc10e35506254
Harry Min <harryhmin@gmail.com>
2024-03-11 23:20:59 +0000

    python don't install ruff_lsp by default
    requires python setup so don't install by default

--- lua/plugins/lang/python.lua
+++ lua/plugins/lang/python.lua
@@ -4,6 +4,7 @@ return {
     opts = {
       servers = {
         ruff_lsp = {
+          mason = false,
           on_attach = function(client, bufnr)
             if client.name == "ruff_lsp" then
               -- Disable hover in favor of Pyright
