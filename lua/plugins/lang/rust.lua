f6491f776c64d6317e2f1a2985fe56aa46609255
Harry Min <harryhmin@gmail.com>
2024-03-11 23:21:15 +0000

    taplo

--- lua/plugins/lang/rust.lua
+++ lua/plugins/lang/rust.lua
@@ -10,6 +10,7 @@ return {
         rust_analyzer = {
           setup = false,
         },
+        taplo = {},
       },
     },
   },
@@ -17,5 +18,17 @@ return {
     "mrcjkb/rustaceanvim",
     version = "^4", -- Recommended
     ft = { "rust" },
+    -- init = function()
+    --   vim.g.rustaceanvim = {
+    --     tools = {
+    --       hover_actions = {
+    --         replace_builtin_hover = false,
+    --       },
+    --       code_actions = {
+    --         ui_select_fallback = true,
+    --       },
+    --     },
+    --   }
+    -- end,
   },
 }
