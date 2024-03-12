a36703a63633b58d147449c7c3405c4ee4b2f314
Harry Min <harryhmin@gmail.com>
2024-03-12 10:39:46 +0000

    material theme

--- lua/plugins/editor/colorscheme.lua
+++ lua/plugins/editor/colorscheme.lua
@@ -7,6 +7,48 @@ return {
     end,
   },
   {
+    "marko-cerovac/material.nvim",
+    opts = {
+      plugins = {
+        "dap",
+        "dashboard",
+        "eyeliner",
+        "fidget",
+        "flash",
+        "gitsigns",
+        "harpoon",
+        "hop",
+        "illuminate",
+        "indent-blankline",
+        "lspsaga",
+        "mini",
+        "neogit",
+        "neotest",
+        "neo-tree",
+        "neorg",
+        "noice",
+        "nvim-cmp",
+        "nvim-navic",
+        "nvim-tree",
+        "nvim-web-devicons",
+        "rainbow-delimiters",
+        "sneak",
+        "telescope",
+        "trouble",
+        "which-key",
+        "nvim-notify",
+      },
+      lualine_style = "stealth",
+      disable = {
+        eob_lines = true,
+      },
+    },
+    init = function()
+      vim.g.material_style = "palenight"
+      vim.cmd.colorscheme("material")
+    end,
+  },
+  {
     "catppuccin/nvim",
     name = "catppuccin",
     opts = {
@@ -16,8 +58,5 @@ return {
         neotree = true,
       },
     },
-    init = function()
-      vim.cmd.colorscheme("catppuccin")
-    end,
   },
 }
