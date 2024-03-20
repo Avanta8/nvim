44881a9a8d63c3addedc3a33631a9aa13651835a
Harry Min <harryhmin@gmail.com>
2024-03-20 23:43:26 +0000

    remove unused plugins and commented code

--- lua/plugins/editor/editor.lua
+++ lua/plugins/editor/editor.lua
@@ -3,58 +3,6 @@ local custom = require("core.custom")
 local utils = require("core.utils")
 
 return {
-  {
-    enabled = false,
-    "akinsho/bufferline.nvim",
-    event = "VeryLazy",
-    lazy = false,
-    keys = {
-      { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
-      { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
-      { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete other buffers" },
-      { "<leader>bL", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete buffers to the right" },
-      { "<leader>bH", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete buffers to the left" },
-      { "<leader>bl", "<CMD>BufferLineCycleNext<CR>", desc = "Next buffer" },
-      { "<leader>bh", "<CMD>BufferLineCyclePrev<CR>", desc = "Prev buffer" },
-      { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev buffer" },
-      { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next buffer" },
-    },
-    opts = {
-      options = {
-        -- stylua: ignore
-        close_command = function(n) require("mini.bufremove").delete(n, false) end,
-        -- stylua: ignore
-        right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
-        diagnostics = "nvim_lsp",
-        diagnostics_indicator = function(_, _, diag)
-          local icons = require("core.custom").icons.diagnostics
-          local ret = (diag.error and icons.Error .. diag.error .. " " or "")
-            .. (diag.warning and icons.Warn .. diag.warning or "")
-          return vim.trim(ret)
-        end,
-        offsets = {
-          {
-            filetype = "neo-tree",
-            text = "Neo-tree",
-            highlight = "Directory",
-            text_align = "left",
-          },
-        },
-      },
-    },
-    config = function(_, opts)
-      require("bufferline").setup(opts)
-      -- Fix bufferline when restoring a session
-      vim.api.nvim_create_autocmd("BufAdd", {
-        callback = function()
-          vim.schedule(function()
-            pcall(nvim_bufferline)
-          end)
-        end,
-      })
-    end,
-  },
-
   -- Preview code in floating window
   {
     "rmagatti/goto-preview",
@@ -119,26 +67,15 @@ return {
         },
         placement = {
           horizontal = "center",
-          -- horizontal = "left",
-          -- vertical = "bottom",
           vertical = "top",
         },
       },
       ignore = {
-        -- floating_wins = false,
         unlisted_buffers = false,
-        -- buftypes = function()
-        --   return false
-        -- end,
-        -- wintypes = function()
-        --   return false
-        -- end,
         filetypes = { "dashboard" },
       },
 
       render = function(props)
-        -- vim.g.count = (vim.g.count or 0) + 1
-        -- print(vim.g.count)
         local colors = require("catppuccin.palettes.macchiato")
 
         local buf = props.buf
@@ -150,7 +87,7 @@ return {
           return #vim.diagnostic.get(props.buf, { severity = vim.diagnostic.severity[severity:upper()] })
         end
 
-        local function start_sec()
+        local function diagnostic_sec()
           local group = "DiagnosticSignOk"
           for _, severity in ipairs({ "Error", "Warn", "Hint", "Info" }) do
             if get_diagnostic_count(severity) > 0 then
@@ -160,27 +97,6 @@ return {
           end
           return {
             { "▌ ", group = group },
-          }
-        end
-
-        local function diagnostics_sec()
-          local icons = require("core.custom").icons.diagnostics
-          local errors = get_diagnostic_count("ERROR")
-          local warnings = get_diagnostic_count("WARN")
-          local error_text = (errors > 0 and icons.Error .. errors or "")
-          local warnings_text = (warnings > 0 and icons.Warn .. warnings or "")
-          -- text = vim.trim(text)
-
-          local error_sec = { error_text, group = "DiagnosticSignError" }
-          local warnings_sec = { warnings_text, group = "DiagnosticSignWarn" }
-          local count = (errors > 0 and 1 or 0) + (warnings > 0 and 1 or 0)
-
-          return {
-            error_sec,
-            { count > 1 and " " or "" },
-
-            warnings_sec,
-            { count > 0 and " " or "" },
           }
         end
 
@@ -221,133 +137,16 @@ return {
           }
         end
 
-        local function end_sec()
-          return {
-            { " ▐", guifg = colors.green },
-          }
-        end
-
         local text_color = focused and colors.rosewater or colors.overlay2
 
         return {
-          start_sec(),
-          -- diagnostics_sec(),
+          diagnostic_sec(),
           name_sec(),
           modified_sec(),
-          -- end_sec(),
           guifg = text_color,
           guibg = colors.crust,
         }
       end,
-
-      -- render = function(props)
-      --   local function get_diagnostic_label(props)
-      --     local icons = {
-      --       Error = "",
-      --       Warn = "",
-      --       Info = "",
-      --       Hint = "",
-      --     }
-      --
-      --     local label = {}
-      --     for severity, icon in pairs(icons) do
-      --       local n = #vim.diagnostic.get(props.buf, { severity = vim.diagnostic.severity[string.upper(severity)] })
-      --       if n > 0 then
-      --         table.insert(label, { icon .. " " .. n .. " ", group = "DiagnosticSign" .. severity })
-      --       end
-      --     end
-      --     return label
-      --   end
-      --
-      --   local bufname = vim.api.nvim_buf_get_name(props.buf)
-      --   local filename = vim.fn.fnamemodify(bufname, ":t")
-      --   local diagnostics = get_diagnostic_label(props)
-      --   local modified = vim.api.nvim_buf_get_option(props.buf, "modified") and "bold,italic" or "None"
-      --   local filetype_icon, color = require("nvim-web-devicons").get_icon_color(filename)
-      --   local colors = require("catppuccin.palettes.macchiato")
-      --
-      --   local buffer = {
-      --     { filetype_icon, guifg = color },
-      --     { " " },
-      --     { filename, gui = modified },
-      --   }
-      --
-      --   if #diagnostics > 0 then
-      --     table.insert(diagnostics, { "| ", guifg = "grey" })
-      --   end
-      --   for _, buffer_ in ipairs(buffer) do
-      --     table.insert(diagnostics, buffer_)
-      --   end
-      --   return diagnostics
-      -- end,
-
-      -- render = function(props)
-      --   local helpers = require("incline.helpers")
-      --   local devicons = require("nvim-web-devicons")
-      --   local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-      --   if filename == "" then
-      --     filename = "[No Name]"
-      --   end
-      --   local ft_icon, ft_color = devicons.get_icon_color(filename)
-      --   local modified = vim.bo[props.buf].modified
-      --   return {
-      --     ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or "",
-      --     " ",
-      --     { filename, gui = modified and "bold,italic" or "bold" },
-      --     " ",
-      --     guibg = "#44406e",
-      --   }
-      -- end,
-
-      -- render = function(props)
-      --   local helpers = require("incline.helpers")
-      --   local navic = require("nvim-navic")
-      --   local devicons = require("nvim-web-devicons")
-      --   local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-      --   if filename == "" then
-      --     filename = "[No Name]"
-      --   end
-      --   local ft_icon, ft_color = devicons.get_icon_color(filename)
-      --   local modified = vim.bo[props.buf].modified
-      --
-      --   local create_navic_context = function()
-      --     local context = {}
-      --     local data = navic.get_data(props.buf) or {}
-      --     for i, item in ipairs(data) do
-      --       table.insert(context, {
-      --         { item.icon, group = "NavicIcons" .. item.type },
-      --         { item.name, group = "NavicText" },
-      --       })
-      --       if i < #data then
-      --         context[#context + 1] = { " > ", group = "NavicSeparator" }
-      --       end
-      --     end
-      --     return context
-      --   end
-      --
-      --   local filename_section = {
-      --     { filename, gui = modified and "bold,italic" or "bold" },
-      --     " ",
-      --     ft_icon and { " ", ft_icon, " ", guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or "",
-      --   }
-      --
-      --   local res = {
-      --     guibg = "#282642",
-      --     " ",
-      --   }
-      --
-      --   if props.focused then
-      --     local context = create_navic_context()
-      --     if #context > 0 then
-      --       res[#res + 1] = context
-      --       res[#res + 1] = " │ "
-      --     end
-      --   end
-      --
-      --   res[#res + 1] = filename_section
-      --
-      --   return res
-      -- end,
     },
     init = function()
       local incline = require("incline")
@@ -362,89 +161,6 @@ return {
         end,
       })
     end,
-  },
-
-  {
-    enabled = false,
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-    keys = function()
-      local command = require("neo-tree.command")
-      return {
-        -- Reveal the current file, or if in an unsaved file, the current working directory.
-        -- Taken from help docs
-        {
-          "<leader>fs",
-          function()
-            local reveal_file = vim.fn.expand("%:p")
-            if reveal_file == "" then
-              reveal_file = vim.fn.getcwd()
-            else
-              local f = io.open(reveal_file, "r")
-              if f then
-                f.close(f)
-              else
-                reveal_file = vim.fn.getcwd()
-              end
-            end
-            command.execute({
-              reveal_file = reveal_file, -- path to file or folder to reveal
-              reveal_force_cwd = true, -- change cwd without asking if needed
-            })
-          end,
-          desc = "Reveal file",
-        },
-        {
-          "<leader>fe",
-          function()
-            command.execute({ dir = vim.loop.cwd() })
-          end,
-          desc = "File explorer (cwd) (focus)",
-        },
-        {
-          "<leader>fE",
-          function()
-            command.execute({ toggle = true, dir = vim.loop.cwd() })
-          end,
-          desc = "File explorer (cwd) (toggle)",
-        },
-        {
-          "<leader>ge",
-          function()
-            command.execute({ source = "git_status" })
-          end,
-          desc = "Git explorer (focus)",
-        },
-        {
-          "<leader>gE",
-          function()
-            command.execute({ source = "git_status", toggle = true })
-          end,
-          desc = "Git explorer (toggle)",
-        },
-        {
-          "<leader>be",
-          function()
-            command.execute({ source = "buffers" })
-          end,
-          desc = "Buffer explorer (focus)",
-        },
-        {
-          "<leader>bE",
-          function()
-            command.execute({ source = "buffers", toggle = true })
-          end,
-          desc = "Buffer explorer (toggle)",
-        },
-      }
-    end,
-    opts = {},
   },
 
   -- Peek line numbers
@@ -497,15 +213,6 @@ return {
 
       map("]]", "next")
       map("[[", "prev")
-
-      -- also set it after loading ftplugins, since a lot overwrite [[ and ]]
-      -- vim.api.nvim_create_autocmd("FileType", {
-      --   callback = function()
-      --     local buffer = vim.api.nvim_get_current_buf()
-      --     map("]]", "next", buffer)
-      --     map("[[", "prev", buffer)
-      --   end,
-      -- })
     end,
     keys = {
       { "]]", desc = "Next Reference" },
@@ -535,13 +242,6 @@ return {
     opts = {
       scope = "line",
     },
-  },
-
-  {
-    enabled = false,
-    "m4xshen/hardtime.nvim",
-    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-    opts = {},
   },
 
   {
@@ -621,22 +321,6 @@ return {
     event = "LspAttach",
     dependencies = {
       "neovim/nvim-lspconfig",
-      {
-        "nvim-lualine/lualine.nvim",
-        -- opts = function(_, opts)
-        --   opts.sections = opts.sections or {}
-        --   opts.sections.lualine_c = opts.sections.lualine_c or {}
-        --   table.insert(opts.sections.lualine_c, {
-        --     -- function()
-        --     --   return require("nvim-navic").get_location()
-        --     -- end,
-        --     -- cond = function()
-        --     --   return package.loaded["nvim-navic"] and require("nvim-navic").is_available()
-        --     -- end,
-        --     "navic",
-        --   })
-        -- end,
-      },
     },
     init = function()
       vim.api.nvim_create_autocmd("LspAttach", {
