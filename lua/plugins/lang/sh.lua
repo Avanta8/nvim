1648d630062fd1038bb0fede41c1cbc2311f837d
Harry Min <harryhmin@gmail.com>
2025-10-30 18:18:32 +0000

    refactor(sh): migrate to using the new LSP API

--- lua/plugins/lang/sh.lua
+++ lua/plugins/lang/sh.lua
@@ -1,7 +1,8 @@
-return require("core.lang_setup").create_config({
-  install = { "shfmt" },
-  format = {
-    sh = { "shfmt" },
-  },
-  bashls = {},
-})
+local lang = require("core.lang_setup")
+
+vim.lsp.enable("bashls")
+
+lang.add_ensure_installed({ "bash-language-server", "shfmt" })
+lang.set_formatters("sh", { "shfmt" })
+
+return {}
