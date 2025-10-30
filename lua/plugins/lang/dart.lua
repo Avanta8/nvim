88123a45aa8c88508a8f66ab3fd0c8658c8e3e53
Harry Min <harryhmin@gmail.com>
2025-10-30 18:18:30 +0000

    refactor(dart): migrate to using the new LSP API

--- lua/plugins/lang/dart.lua
+++ lua/plugins/lang/dart.lua
@@ -1,17 +1,9 @@
+-- dartls is not available in the mason registry so it needs to
+-- be installed manually
+--
+-- dartls setup is handled by flutter-tools.nvim
+
 return {
-  require("core.lang_setup").create_config({
-    -- Dart format is really slow so don't use it.
-    -- Defer formatting to LSP instead
-
-    -- Could just not provide this as dartls as it isn't currently available by mason.
-    -- But this is more "future proof"! (in case it does become available by mason and
-    -- we somehow accidently install it by mason)
-    dartls = {
-      mason = false,
-      setup = false,
-    },
-  }),
-
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
