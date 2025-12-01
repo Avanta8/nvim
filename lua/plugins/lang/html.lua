81f62207462eed7698a3c63b945c3bd0b2de1609
Harry Min <harryhmin@gmail.com>
2025-12-01 16:03:29 +0000

    feat(html): root-based activation for jinja_lsp and djlint
    - Enable `jinja_lsp` only if `jinja-lsp.toml` is present in the project root
    - Run `djlint` formatter only if `djlint.toml` or `.djlintrc` is found

--- lua/plugins/lang/html.lua
+++ lua/plugins/lang/html.lua
@@ -21,6 +21,16 @@ lang.add_ensure_installed({ "jinja-lsp" })
 
 vim.lsp.config.jinja_lsp = {
   filetypes = { "jinja", "htmldjango", "html" },
+  -- Only actiavate jinja_lsp if a jinja-lsp.toml file is found
+  root_dir = function(bufnr, on_dir)
+    local lspconfig = require("lspconfig")
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    local root_dir = lspconfig.util.root_pattern("jinja-lsp.toml")(fname)
+
+    if root_dir then
+      on_dir(root_dir)
+    end
+  end,
 }
 
 vim.lsp.enable({ "jinja_lsp" })
@@ -30,4 +40,21 @@ lang.set_formatters("htmldjango", { "djlint" })
 lang.set_formatters("html", { "djlint" })
 lang.set_formatters("jinja", { "djlint" })
 
-return {}
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      local conform_util = require("conform.util")
+      local extend_opts = {
+        formatters = {
+          djlint = {
+            require_cwd = true,
+            -- Only run djlint if a djlint.toml or .djlintrc file is found
+            cwd = conform_util.root_file({ "djlint.toml", ".djlintrc" }),
+          },
+        },
+      }
+      return vim.tbl_deep_extend("force", opts, extend_opts)
+    end,
+  },
+}
